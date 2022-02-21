19c82aac24b92aa3fcddf0d543ebfcac202aee3f
Somashekar9999 <97974767+Somashekar9999@users.noreply.github.com>
2022-02-21 18:11:27 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+function sidebar() {
+
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+    return `   <h2>menu</h2>
+    <h2>login</h2>
+    <h2>sign Up</h2>
+    <input type="text" placeholder="Search" id="searchbar">`
+}
+export default sidebar
